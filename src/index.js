7de9dd10a7d1b4124fdbde6b3bc7b64b728f8e91
Hyunjoon Choi <90085154+devholic22@users.noreply.github.com>
2022-11-08 17:12:36 +0900

    기본 설정

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import dotenv from "dotenv";
+dotenv.config();
+import express from "express";
+import { createConnection } from "./models/index.js";
+import { globalRouter } from "./routers/globalRouter.js";
+
+const app = express();
+const PORT = 8080;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+createConnection();
+
+app.use(globalRouter);
+
+const handleListen = () => {
+  console.log(`✅ Server listening at: http://localhost:${PORT}`);
+};
+
+app.listen(PORT, handleListen);
---
+++ src/models/index.js
@@ -0,0 +1,30 @@
+import { join } from "path";
+import { Low, JSONFile } from "lowdb";
+
+export let db;
+
+export const createConnection = async () => {
+  // Use JSON file for storage
+  const file = join(process.cwd(), "src/db/db.json");
+  const adapter = new JSONFile(file);
+  db = new Low(adapter);
+
+  // Read data from JSON file, this will set db.data content
+  await db.read();
+
+  // If file.json doesn't exist, db.data will be null
+  // Set default data
+  // db.data = db.data || { posts: [] } // Node < v15.x
+  db.data ||= { users: [] }; // Node >= 15.x
+
+  // Create and query items using plain JS
+  // db.data.posts.push("hello world");
+  // const firstPost = db.data.posts[0];
+
+  // Alternatively, you can also use this syntax if you prefer
+  // const { posts } = db.data;
+  // posts.push("hello world");
+
+  // Finally write db.data content to file
+  await db.write();
+};
