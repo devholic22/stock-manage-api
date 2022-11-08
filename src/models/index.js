f005c3e3a2a59d0a12768f5089a663860e8f290a
Hyunjoon Choi <90085154+devholic22@users.noreply.github.com>
2022-11-08 17:51:13 +0900

    error fix

--- src/models/index.js
+++ src/models/index.js
@@ -1,25 +1,31 @@
-// Remember to set type: module in package.json or use .mjs extension
-import { join, dirname } from "node:path";
-import { fileURLToPath } from "node:url";
+import { join } from "path";
+import { Low, JSONFile } from "lowdb";
 
-import { Low } from "lowdb";
-import { JSONFile } from "lowdb/node";
+export let db;
 
-// File path
 export const createConnection = async () => {
+  // Use JSON file for storage
+  console.log(process.cwd());
   const file = join(process.cwd(), "src/db/db.json");
-
-  // Configure lowdb to write to JSONFile
+  console.log(file);
   const adapter = new JSONFile(file);
-  const db = new Low(adapter);
+  db = new Low(adapter);
 
   // Read data from JSON file, this will set db.data content
   await db.read();
 
-  // If db.json doesn't exist, db.data will be null
-  // Use the code below to set default data
-  // db.data = db.data || { posts: [] } // For Node < v15.x
-  db.data ||= {}; // For Node >= 15.x
+  // If file.json doesn't exist, db.data will be null
+  // Set default data
+  // db.data = db.data || { posts: [] } // Node < v15.x
+  db.data ||= {}; // Node >= 15.x
+
+  // Create and query items using plain JS
+  // db.data.posts.push("hello world");
+  // const firstPost = db.data.posts[0];
+
+  // Alternatively, you can also use this syntax if you prefer
+  // const { posts } = db.data;
+  // posts.push("hello world");
 
   // Finally write db.data content to file
   await db.write();
