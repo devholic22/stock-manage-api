ac33c581ae06c33c1b179366c3d1955f0a3ed164
Hyunjoon Choi <90085154+devholic22@users.noreply.github.com>
2022-12-10 22:58:30 +0900

    Update Stock.js

--- src/models/Stock.js
+++ src/models/Stock.js
@@ -7,6 +7,7 @@ class Stock {
     this.number = db.data?.company[type.company - 1].stocks + 1;
     this.numberInType =
       db.data?.company[type.company - 1].types[type.number - 1].size + 1;
+    this.type = type.number - 1;
     this.origin = origin; // 제조사
     this.name = name; // 품명
     this.size = size; // 규격
@@ -70,6 +71,7 @@ class Stock {
       return null;
     }
     result = curr;
+    console.log(result.type);
     // delete result["next"];
     // delete result["company"];
     return result;
